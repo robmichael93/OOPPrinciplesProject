bb31ce3b031222a15abc5b72a7d724caa9948f27
Rob Michael <robmichael93@gmail.com>
2024-09-22 23:43:15 -0500

    Update starter files & initial scripts
    Update to the added starter files, moving some from the challenge to the game assets.  Set up initial base class & children for next iteration.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,37 +7,82 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> targets;
-    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI matchesText;
+    public TextMeshProUGUI timerText;
     public TextMeshProUGUI gameOverText;
     public GameObject titleScreen;
-    public Button restartButton;
-    private int score;
-    private float spawnRate = 2;
-     public bool isGameActive;
+    public Button restartButton; 
 
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> targetPrefabs;
+
+    private int matchesMade;
+    private float time;
+    public bool isGameActive;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int difficulty)
     {
-
+        isGameActive = true;
+        matchesMade = 0;
+        time = 0;
+        UpdateTime(time);
+        titleScreen.SetActive(false);
+        SpawnBoxes();
     }
 
-    IEnumerator SpawnTarget()
+    private void Update()
     {
-        while (isGameActive)
+        if (isGameActive)
         {
-        yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
+            CountupTimer();    
         }
     }
 
-    public void UpdateScore(int scoreToAdd)
+    // While game is active spawn a random target
+    void SpawnBoxes()
     {
-        score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        int index = Random.Range(0, targetPrefabs.Count);
+
+        if (isGameActive)
+        {
+            Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+        }
     }
 
+    // Generate a random spawn position based on a random index from 0 to 3
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    // Update score with value from target clicked
+    public void UpdateMatches()
+    {
+        matchesMade++;
+        matchesText.text = "Matches: " + matchesMade;
+    }
+
+    void UpdateTime(float currentTime)
+    {
+        timerText.text = "Time: " + Mathf.FloorToInt(currentTime % 60);
+    }
+
+    // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
@@ -45,23 +90,16 @@ public class GameManager : MonoBehaviour
         isGameActive = false;
     }
 
+    void CountupTimer()
+    {
+        time += Time.deltaTime;
+        UpdateTime(time);
+    }
+
+    // Restart game by reloading the scene
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void StartGame(int difficulty)
-    {
-        isGameActive = true;
-        score = 0;
-        spawnRate /= difficulty;
-        titleScreen.gameObject.SetActive(false);
-        StartCoroutine(SpawnTarget());
-        UpdateScore(score);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
 }
